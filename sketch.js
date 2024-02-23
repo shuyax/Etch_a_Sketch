60900f9413a47bf3bd73339a11b7c6da069833c0
Mac Mini <macmini@macs-mbp.myfiosgateway.com>
2024-02-23 17:30:06 -0500

    Change the css file to make sure when user change the number of grids on each row and column, the container will be updated without changing the size of the container

--- sketch.js
+++ sketch.js
@@ -1,17 +1,24 @@
 const container = document.querySelector('.flex-container')
-
 function createGrid(rows, columns){
     for (let i = 0; i < rows; i++ ){
         const row = document.createElement('div')
+        row.classList.add('row')
         container.appendChild(row)
         for (let j = 0; j < columns; j++){
             const grid = document.createElement('div')
             grid.classList.add('grid-item')
-            grid.style.border = '1px solid blue'
             row.appendChild(grid)
-            console.log(`Created grid at (${i}, ${j})`);
+            // console.log(`Created grid at (${i}, ${j})`);
         }
         
     }
 }
-createGrid(16,16)
+
+
+createGrid(40,40)
+const grids = document.querySelectorAll('.grid-item')
+grids.forEach(grid => {
+    grid.addEventListener('mouseover',function (e) {
+        e.target.style.backgroundColor = 'black'
+    } )
+})
